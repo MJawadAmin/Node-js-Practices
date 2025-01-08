16f5eb806abe202095d8fd2f91fb8e8c1391ea63
Jaddikhan <114812094+Jaddikhan@users.noreply.github.com>
2025-01-08 10:54:32 +0500

    Functions and calling export practices

--- Hello.js
+++ Hello.js
@@ -1,1 +1,3 @@
-console.log("Hey this is my first Node js program")
+const {add, sub} =require("./math")
+console.log("Hey this is my first Node js program" , add(2,4))
+console.log("This is my second Node js program" , sub(5,2))
