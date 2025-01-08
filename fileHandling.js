3475bf05297e5a40085a525f798c2749bd76345b
Jaddikhan <114812094+Jaddikhan@users.noreply.github.com>
2025-01-08 12:57:25 +0500

    File Handling practice has been completed

--- fileHandling.js
+++ fileHandling.js
@@ -20,6 +20,26 @@ const fs =require ("fs");
     //     console.log(data);
     // }});
 
-    // error in line but remember that      fs.appendFileSync('./text.txt' , newDate().getDate().toLocaleString()  ); append 
-   fs.appendFileSync ('./text.txt' , new Date().getTime().toLocaleString())
-
+    // error in line but remember that      fs.appendFileSync('./text.txt' , newDate().getDate().toLocaleString()  ); 
+//     // Append time 
+//    fs.appendFileSync ('./text.txt' , new Date().getTime().toLocaleString())
+// Append Date
+// fs.appendFileSync('./text.txt' , new Date().getDate().toLocaleString())
+// Append String
+// fs.appendFileSync('./text.txt' , ("Hey there are new text \n"))
+// // date
+// fs.appendFileSync('./text.txt', `${Date.now} hey there \n`);
+//copy File 
+// fs.copyFileSync('./text.txt' , './text2.txt')
+// Delete File
+// fs.unlinkSync('./text2.txt')
+// File state
+// console.log(fs.statSync('./text.txt'))
+// console.log(fs.statSync('./text.txt').isFile)
+// fs.mkdirSync ('./newFolder');
+// delete the folder
+//  fs.unlinkSync ('./newFolder');
+// multi Folders
+fs.mkdirSync("newffolder/a/b/c",{
+    recursive: true
+})
